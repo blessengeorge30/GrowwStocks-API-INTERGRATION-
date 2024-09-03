e9754185b4a434261cfd1937f090ef026df2a6f3
Blessen George <blessen.george30@gmail.com>
2024-09-03 16:11:54 +0530

    U.I Modification

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -177,7 +177,7 @@ export default function HomeScreen({ navigation }) {
           </TouchableOpacity>
         </View>
         <View style={styles.toggleContainer}>
-          <Text style={[styles.label, isDarkMode && styles.textWhite]}>Light</Text>
+          <Text style={[styles.label, isDarkMode && styles.textWhite]}>Light mode</Text>
           <Switch
             value={isDarkMode}
             onValueChange={toggleTheme}
@@ -186,7 +186,7 @@ export default function HomeScreen({ navigation }) {
             ios_backgroundColor="#3e3e3e"
             style={styles.switch}
           />
-          <Text style={[styles.label, isDarkMode && styles.textWhite]}>Dark </Text>
+          <Text style={[styles.label, isDarkMode && styles.textWhite]}>Dark mode</Text>
         </View>
       </View>
 
